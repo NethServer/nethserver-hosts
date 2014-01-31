6e1231671a6ea1ff8411304fe1c7fa1accc00154
Davide Principi <davide.principi@nethesis.it>
2014-01-31 15:27:54 +0100

    Fixed Alias and DNS form headers. Refs #2642
    Added also Help buttons.

--- root/usr/share/nethesis/NethServer/Template/Hosts/Alias.php
+++ root/usr/share/nethesis/NethServer/Template/Hosts/Alias.php
@@ -1,9 +1,19 @@
 <?php
+/* @var $view \Nethgui\Renderer\Xhtml */
+
+if ($view->getModule()->getIdentifier() == 'update') {
+    $keyFlags = $view::STATE_DISABLED;
+    $template = 'Update_Alias_Header';
+} else {
+    $keyFlags = 0;
+    $template = 'Create_Alias_Header';
+}
+
+echo $view->header('hostname')->setAttribute('template', $T($template));
 
 echo $view->panel()
-        ->insert($view->header('address')->setAttribute('template', 'Host entry'))
-        ->insert($view->textInput('hostname'))
+        ->insert($view->textInput('hostname', $keyFlags))
         ->insert($view->textInput('Description'))
 ;
         
-echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL);
+echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL | $view::BUTTON_HELP);
--- root/usr/share/nethesis/NethServer/Template/Hosts/Dns.php
+++ root/usr/share/nethesis/NethServer/Template/Hosts/Dns.php
@@ -1,10 +1,20 @@
 <?php
+/* @var $view \Nethgui\Renderer\Xhtml */
+
+if ($view->getModule()->getIdentifier() == 'update') {
+    $keyFlags = $view::STATE_DISABLED;
+    $template = 'Update_Dns_Header';
+} else {
+    $keyFlags = 0;
+    $template = 'Create_Dns_Header';
+}
+
+echo $view->header('hostname')->setAttribute('template', $T($template));
 
 echo $view->panel()
-        ->insert($view->header('address')->setAttribute('template', 'Host entry'))
-        ->insert($view->textInput('hostname', ($view->getModule()->getIdentifier() == 'update' ? $view::STATE_READONLY : 0)))
+        ->insert($view->textInput('hostname', $keyFlags))
         ->insert($view->textInput('IpAddress'))
         ->insert($view->textInput('Description'))
 ;
         
-echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL);
+echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL | $view::BUTTON_HELP);
