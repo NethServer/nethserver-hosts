53d55e4af74efe97e7ca0e40cbccdf21c018ee0f
Davide Principi <davide.principi@nethesis.it>
2014-12-16 16:11:46 +0100

    Configure UI page: removed Cancel button.

--- root/usr/share/nethesis/NethServer/Template/Hosts/Configure.php
+++ root/usr/share/nethesis/NethServer/Template/Hosts/Configure.php
@@ -5,4 +5,4 @@ echo $view->header()->setAttribute('template', $T('Dns_Configure_header'));
 echo $view->textInput('Dns1', 'disabled')->setAttribute('label', $T('dns1_label'));
 echo $view->textInput('Dns2', 'disabled')->setAttribute('label', $T('dns2_label'));
 
-echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL | $view::BUTTON_HELP);
+echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
