00a4a1955258874d3e06f077bb705caef38631cf
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2015-11-24 17:50:58 +0100

    Web UI: pass 'hosts' parameter to host-delete validator. Refs #3173

--- root/usr/share/nethesis/NethServer/Module/Hosts/Dns/Modify.php
+++ root/usr/share/nethesis/NethServer/Module/Hosts/Dns/Modify.php
@@ -46,7 +46,7 @@ class Modify extends \Nethgui\Controller\Table\Modify
     public function validate(\Nethgui\Controller\ValidationReportInterface $report)
     {
         if ($this->getIdentifier() === 'delete') {
-            $v = $this->createValidator()->platform('host-delete');
+            $v = $this->createValidator()->platform('host-delete', 'hosts');
             if ( ! $v->evaluate($this->parameters['hostname'])) {
                 $report->addValidationError($this, 'Key', $v);
             }
